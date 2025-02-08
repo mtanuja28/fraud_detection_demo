56a1b41a5b1d19a787b8eed41091be8bc40ce517
mtanuja28 <tanujamodupalli@gmail.com>
2025-02-08 17:43:13 +0530

    Model 3 is final model

--- model_building.py
+++ model_building.py
@@ -1,1 +1,12 @@
 print("All model related code is written here")
+
+def model1():
+    print("MOdel 1")
+
+def model2():
+    print("Model2")
+
+def model3():
+    print("model3")
+
+print("model 3 is the final model which is working properly acc to expectation")
